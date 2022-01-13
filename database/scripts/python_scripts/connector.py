d8c4b7581a97620665854d43a0259dfe810caa71
malisyed <zcabasy@ucl.ac.uk>
2022-01-13 16:30:08 +0000

    flask setup for use case 1 + dockerised web app

--- database/scripts/python_scripts/connector.py
+++ database/scripts/python_scripts/connector.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
+    host = "127.0.0.1:33061",
+    user = "some-mysql2",
     password = "my-secret-pw"
 )
 
-mycursor = db.cursor()
+#mycursor = db.cursor()
 
 #database = "MovieLensDB"
---
+++ web_app/app/web_app_main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+@app.route("/use-case-1")
+def use_case_1():
+    return render_template("use-case-1/use-case-1.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host="localhost")
