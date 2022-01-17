1f6210c0d55173eeea6f8a7a6bee1f1f82fce843
Karan Chawla <karan.chawla.19@ucl.ac.uk>
2022-01-17 12:10:40 +0000

    Add entrypoint scripts for containerised DB

--- webapp/db-scripts/init.sql
+++ webapp/db-scripts/init.sql
@@ -1,3 +1,10 @@
+CREATE DATABASE IF NOT EXISTS `MovieLensDB`;
+
+USE `MovieLensDB`;
+
+CREATE SCHEMA IF NOT EXISTS `movies_schema` DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA IF NOT EXISTS `user_personality_schema` DEFAULT CHARACTER SET utf8;
+
 USE `movies_schema`;
 
 --Table Movies
@@ -53,3 +60,15 @@ USE `user_personality_schema`;
 --Personality table
 
 --Personality-Ratings
+
+USE `movies_schema`;
+
+LOAD DATA INFILE '/data/movies.csv' INTO TABLE Movies FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA INFILE '/data/tags.csv' INTO TABLE Tags FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA INFILE '/data/ratings.csv' INTO TABLE Ratings FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA INFILE '/data/links.csv' INTO TABLE Links FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+USE `user_personality_schema`;
+
+LOAD DATA INFILE '/data/personality_data/personality-data.csv' INTO TABLE Personality FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA INFILE '/data/personality_data/ratings.csv' INTO TABLE Personality-Ratings FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
