f0a548e89c491b86b9074b7840d035584b5b802b
malisyed <zcabasy@ucl.ac.uk>
2022-01-20 18:20:45 +0000

    added caching framework

--- webapp/microservices/use-case-1/use-case-1.py
+++ webapp/microservices/use-case-1/use-case-1.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_caching import Cache
 
 use_case_1 = Flask(__name__)
 
+cache = Cache()
+cache.init_app(use_case_1)
+use_case_1.config['CACHE_TYPE'] = 'memcached'
+
 @use_case_1.route("/")
+@cache.memoize(timeout=60)
 def home():
     return "1"
 
